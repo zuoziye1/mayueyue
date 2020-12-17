00648df7e1e48f1f4d399c56e98af3cb563018fd
yaofeihu <admin@admindeMacBook-Pro.local>
2020-12-17 22:43:07 +0800

    mayueyue1

---
+++ src/main/java/com/mayueyue/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.mayueyue.controller;
+
+import com.mayueyue.model.User;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Hello {
+
+    public List<User> getUser(){
+        return null;
+    }
+
+}
