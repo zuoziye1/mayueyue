c4c20d39f50e4dec71de27918afa98794a8659d3
zuoziye1 <478251468@qq.com>
2020-12-18 00:20:31 +0800

    test

--- src/main/java/com/mayueyue/mapper/UserMapper.java
+++ src/main/java/com/mayueyue/mapper/UserMapper.java
@@ -4,13 +4,12 @@ import com.mayueyue.model.User;
 import org.springframework.stereotype.Repository;
 
 /**
- *---------
+ *---------trtrtrt 5454
  * @Author: 姚飞虎
  * @Date: 2020/12/17 7:30 下午
  * @Description:
  */
 public interface UserMapper {
-
 
     User getNameById(long userId);
 }
