06c61bc48b0665996896aafc5fcc5c6dd76aa3a9
mayueyue <2608344112@qq.com>
2020-12-28 22:48:03 +0800

    Merge branch 'main' of H:\springboot\mayueyue with conflicts.

--- src/main/java/com/mayueyue/controller/UserController.java
+++ src/main/java/com/mayueyue/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.mayueyue.controller;
 
 import com.mayueyue.mapper.UserMapper;
-import com.mayueyue.model.BaseResult;
 import com.mayueyue.model.User;
-import com.mayueyue.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 /**
  @Author: 马月月
@@ -17,17 +13,32 @@ import javax.annotation.Resource;
  */
 @RestController
 public class UserController {
-
-    @Resource
-    UserService userService ;
+    @Autowired
+    private UserMapper userMapper;
 
     @RequestMapping("/register")
-    public BaseResult register(String name, String account, String password, String telephone){
-        return userService.regist(name,account,password,telephone);
-    }
+    public void register(long userId,String name,String account,String password,String telephone){
+        // 判断数据库里是否有这个名字
+        int count = userMapper.checkName(name);
 
+        if(count > 0){
+            System.out.println("你注册的昵称已经存在");
+
+        }else{
+            User user=new User();
+            user.setName(name);
+            user.setPassword(password);
+            user.setTelephone(telephone);
+            user.setAccount(account);
+            userMapper.add(user);
+
+        }
+
+    }
+    @RequestMapping("/login")
     public void login(String account,String password){
         User user=new User();
+
         if(account.equals(user.getAccount())&&password.equals(user.getPassword())){
             System.out.println("登陆成功");
 
